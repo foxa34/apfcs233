a5045c4d36b9bbe406ab6d96d4f4004ce562c6bc
foxa34 <70354371+foxa34@users.noreply.github.com>
2020-08-27 20:37:07 -0500

    Update blackbox_tb.v

--- blackbox_tb.v
+++ blackbox_tb.v
@@ -1,24 +1,24 @@
 module blackbox_test;
 
-    reg  q, i, f;
+    reg  i, h, o;
 
-    wire u;  
+    wire j;  
 
-    blackbox b1(u, q, i, f);
+    blackbox b1(j, i, h, o);
 
     initial begin
 
         $dumpfile("blackbox.vcd");
         $dumpvars(0, blackbox_test);
 
-        q = 0; i = 0; f = 0; #10;
-        q = 0; i = 0; f = 1; #10;
-        q = 0; i = 1; f = 0; #10;
-        q = 0; i = 1; f = 1; #10;
-        q = 1; i = 0; f = 0; #10;
-        q = 1; i = 0; f = 1; #10;
-        q = 1; i = 1; f = 0; #10;
-        q = 1; i = 1; f = 1; #10;
+        i = 0; h = 0; o = 0; #10;
+        i = 0; h = 0; o = 1; #10;
+        i = 0; h = 1; o = 0; #10;
+        i = 0; h = 1; o = 1; #10;
+        i = 1; h = 0; o = 0; #10;
+        i = 1; h = 0; o = 1; #10;
+        i = 1; h = 1; o = 0; #10;
+        i = 1; h = 1; o = 1; #10;
 
         $display("test complete");
 
