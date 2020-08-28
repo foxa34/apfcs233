31fb52cf2f18cb293994bb2f28db1c1ebb052181
foxa34 <70354371+foxa34@users.noreply.github.com>
2020-08-27 20:14:29 -0500

    Update blackbox_tb.v

--- blackbox_tb.v
+++ blackbox_tb.v
@@ -11,10 +11,10 @@ module blackbox_test;
         $dumpfile("bb.vcd");
         $dumpvars(0, blackbox_test);
 
-        i = 0 f = 0; #10;
-        i = 0 f = 1; #10;
-        i = 1 f = 0; #10;
-        i = 1 f = 1; #10;
+        i = 0; f = 0; #10;
+        i = 0; f = 1; #10;
+        i = 1; f = 0; #10;
+        i = 1; f = 1; #10;
 
         $display("test complete");
 
